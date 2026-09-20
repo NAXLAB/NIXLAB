24120541937e184647f88c96bea857d786883109
NAX LAB <jamesmcdowell@naxlab.net>
2026-09-20 13:08:54 -0400

    DMS Updates

--- flake.nix
+++ flake.nix
@@ -82,7 +82,6 @@
         ./nax/tty/tty.nix
         ./nax/shell/shell.nix
         ./nax/plasticity/plasticity.nix
-        ./nax/music/sung/sung.nix
         
         agenix.nixosModules.default
         nix-flatpak.nixosModules.nix-flatpak
